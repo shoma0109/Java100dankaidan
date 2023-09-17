4d271d472168a13729332f8e71d1acf42194a011
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-17 11:35:53 +0900

    問7回答

--- java100dankaidan/src/Main.java
+++ java100dankaidan/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        new Hello(){
-        };
+        Hello hello = new Hello();
+        }
 //        System.out.print("Hello World");
     }
-}
 
 class Hello {
     public static void show(){
