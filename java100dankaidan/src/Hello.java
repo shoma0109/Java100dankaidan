2a995c5e9d288fc68ba0b444f5aae7a385317aef
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-17 13:17:09 +0900

    問12回答

--- java100dankaidan/src/Hello.java
+++ java100dankaidan/src/Hello.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 class Hello {
-    String askName;
+    String lastName;
     public void show() {
         System.out.println("Hello world");
     }
     public void askName(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("名前を入力してください");
+    }
+    public void firstName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("苗字を入力してください");
     }
 }
