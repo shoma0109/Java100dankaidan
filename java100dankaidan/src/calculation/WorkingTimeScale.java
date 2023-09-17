e5f7dffaee657ac946b381d27a7c5643d0884a45
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-17 20:28:42 +0900

    addメソッド内でフィールド変数合計時間を加算する処理の追加

--- java100dankaidan/src/calculation/WorkingTimeScale.java
+++ java100dankaidan/src/calculation/WorkingTimeScale.java
@@ -4,7 +4,8 @@ import java.math.BigDecimal;
 public class WorkingTimeScale {
     private final BigDecimal time1 = new BigDecimal("10");
     private final BigDecimal time2 = new BigDecimal("0.1256789");
+    BigDecimal result = time1.add(time2);
     public void add(int num1, int num2){
-
+        System.out.println(result);
     }
 }
