01aac5452a105a1a46629c116d9c062c90432a7c
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-17 20:12:58 +0900

    addメソッドの追加

--- java100dankaidan/src/calculation/WorkingTimeScale.java
+++ java100dankaidan/src/calculation/WorkingTimeScale.java
@@ -2,5 +2,9 @@ package calculation;
 import java.math.BigDecimal;
 
 public class WorkingTimeScale {
-    private final BigDecimal time1 = new BigDecimal(10);
+    private final BigDecimal time1 = new BigDecimal("10");
+    private final BigDecimal time2 = new BigDecimal("0.1256789");
+    public void add(int num1, int num2){
+
+    }
 }
