07502160bbee80a660c54c65342246459ed41d8d
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 16:10:20 +0900

    Calcファイル削除

--- java100dankaidan/src/calculation/Calc.java
+++
@@ -1,9 +0,0 @@
-package calculation;
-
-public class Calc {
-    public static void main(String[] args){
-        WorkingTimeScale workingTimeScale = new WorkingTimeScale();
-        workingTimeScale.add((int) 10.1256789);
-//        workingTimeScale.currentWorkingTime();
-    }
-}
