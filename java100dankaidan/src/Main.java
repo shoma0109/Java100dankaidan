ff1a7013c8ae6a6e227bc368f17ce44a4b2f108d
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 17:57:15 +0900

    addメソッドを呼び出す処理の追加

--- java100dankaidan/src/Main.java
+++ java100dankaidan/src/Main.java
@@ -1,10 +1,13 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args){
         Hello hello = new Hello();
         hello.askName();
         hello.show();
         WorkingTimeScale workingTimeScale = new WorkingTimeScale();
-        workingTimeScale.add(0);
-        workingTimeScale.currentWorkingTime();
+        var number = 10.1256789;
+        workingTimeScale.add(BigDecimal.valueOf(number));
+//        workingTimeScale.currentWorkingTime();
     }
 }
--- java100dankaidan/src/WorkingTimeScale.java
+++ java100dankaidan/src/WorkingTimeScale.java
@@ -2,12 +2,13 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class WorkingTimeScale {
-    private BigDecimal total_time = new BigDecimal("10.1256789");
-    public void add(int num){
+    private BigDecimal total_time = new BigDecimal(0);
+    public void add(BigDecimal num){
+        System.out.println(num);
     }
 
-    public void currentWorkingTime(){
-        BigDecimal result = total_time.setScale(2, RoundingMode.DOWN);
-        System.out.println(result);
-    }
+//    public void currentWorkingTime(){
+//        BigDecimal result = total_time.setScale(2, RoundingMode.DOWN);
+//        System.out.println(result);
+//    }
 }
