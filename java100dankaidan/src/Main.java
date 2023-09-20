31f448dd847ae202faeaadc2df01a9028c0a2717
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-20 20:58:57 +0900

    経過時間の算出方法、四捨五入⇒切り捨て

--- java100dankaidan/src/Main.java
+++ java100dankaidan/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
 //        hello.show();
         WorkingTimeScale workingTimeScale = new WorkingTimeScale();
         var number = 10.1256789;
-        LocalDateTime from = LocalDateTime.of(2023,9,1,0,0);
-        LocalDateTime to = LocalDateTime.of(2023,9,1,8,16);
+        LocalDateTime from = LocalDateTime.of(2023,9,1,0,0,0);
+        LocalDateTime to = LocalDateTime.of(2023,9,1,8,16,59);
         workingTimeScale.add(BigDecimal.valueOf(number));
         workingTimeScale.add(BigDecimal.valueOf(number));
         workingTimeScale.add(BigDecimal.valueOf(number));
